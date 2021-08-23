d15b25f6d1a2fdc7cac29012696988f25cec63f3
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-23 12:58:39 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,25 @@
-
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    
+    edad = int(input("Ingresa tu edad: "))
+    # Escribe el código adecuado para completar el programa
+    # Para pedir el dato de la idetificación oficial emplea este mensaje:
+    # "¿Tienes identificación oficial? (s/n): "
+    if edad>=18:
+        iden=str(input('¿Tienes identificación oficial? (s/n): '))
+        if  iden=='s':
+            print('Trámite de licencia concedido')
+        elif iden=='n':
+            print('No cumples requisitos')
+
+        else:
+            print('Respuesta incorrecta')
+    
+    elif edad<18:
+        print('No cumples requisitos')
+
+    else:
+        print('Respuesta incorrecta')
+
 
 
 if __name__ == '__main__':
