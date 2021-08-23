33c5c25d8c0777beea4bf6841f352c54f93f90a6
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-23 14:46:26 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,28 @@
 def main():
-    #escribe tu código abajo de esta línea
+    # Escribe el código adecuado para completar el programa
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+
+    if peso>0 and altura>0:
+
+        indi= peso/(altura**2)
+
+        if indi<20:
+            print('PESO BAJO')
+        elif 20<=indi<25:
+            print('NORMAL')
+        elif 25<=indi<30:
+            print('SOBREPESO')
+        elif 30<=indi<40:
+            print('OBESIDAD')
+        elif indi>=40:
+            print('OBESIDAD MORBIDA')
+
+    
+    else:
+        print('Revisa tus datos, alguno de ellos es erróneo.')
+
     pass
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
